f85f1e6996a498fcf88a934b3853abf98c306b67
Muhammad Fahd Al Islam Al Bantani <145535438+fahdaja@users.noreply.github.com>
2026-03-03 13:23:44 +0700

    Feat/gigs (#6)
    * feat(merchants): add update profile merchants
    
    * refactor(merchant): code change
    
    * fix(merchants): fix conflicts code
    
    * fix(merchant): new fix
    
    * feat(merchants): no bug fix
    
    * feat(gigs): add feature approve & reject gigs

--- src/gigs/gigs.controller.ts
+++ src/gigs/gigs.controller.ts
@@ -1,28 +1,34 @@
-import { Controller, Post, Get, Body, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { GigsService } from './gigs.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { CreateGigDto } from './gigs.dto';
+
+interface RequestWithUser extends Request {
+  user: {
+    sub: number;
+    role: string;
+  };
+}
 
 @Controller('gigs')
 export class GigsController {
-constructor(private gigsService: GigsService) {}
+  constructor(private gigsService: GigsService) {}
 
-@UseGuards(AuthGuard)
-@Post()
-create(
-    @Request() req, 
-    @Body() body: { categoryId: number; title: string; description: string; price: number } 
-) {
-    return this.gigsService.createGig(
-        req.user.sub,      
-        body.categoryId,   
-        body.title,        
-        body.description,  
-        body.price         
-    );
-    }
+  @UseGuards(AuthGuard)
+  @Post()
+  create(@Request() req: RequestWithUser, @Body() dto: CreateGigDto) {
+    return this.gigsService.createGig(req.user.sub, dto);
+  }
 
-@Get()
-findAll() {
-    return this.gigsService.findAllGigs();
-}
+  @Get()
+  findAll() {
+    return this.gigsService.findAllActiveGigs();
+  }
 }
